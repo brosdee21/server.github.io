e2f9d5a3e63efe8d50f4efc68936202984a829dd
brosdee21 <nwaokolodennis90@gmail.com>
2025-04-11 16:43:52 +0100

    Initial commit

---
+++ models/Jobs.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+  company: String,
+  role: String,
+  status: String,
+  appliedDate: String,
+  link: String,
+});
+
+module.exports = mongoose.model('Job', jobSchema);
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const Job = require('./models/Jobs');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'));
+
+app.get('/api/jobs', async (req, res) => {
+  const jobs = await Job.find();
+  res.json(jobs);
+});
+
+app.post('/api/jobs', async (req, res) => {
+  const newJob = await Job.create(req.body);
+  res.json(newJob);
+});
+
+app.put('/api/jobs/:id', async (req, res) => {
+  const updatedJob = await Job.findByIdAndUpdate(req.params.id, { status: req.body.status }, { new: true });
+  res.json(updatedJob);
+});
+
+app.delete('/api/jobs/:id', async (req, res) => {
+  await Job.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Job deleted' });
+});
+
+app.listen(5000, () => console.log('Server running on http://localhost:5000'));
